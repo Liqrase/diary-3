e932f5b6d0c041b0e53dcf589fedb234ee6b2026
Liqrase <qarnqitana9w4@gmail.com>
2025-03-26 02:07:06 +0900

    250326-a

--- assets/js/open-external-links.js
+++ assets/js/open-external-links.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("a").forEach((link) => {
-      const targetUrl = "https://liqrase.github.io/cafe/"; // 例外として現在のタブで開くURL
-
-      if (link.hostname !== window.location.hostname && link.href !== targetUrl) {
-          link.setAttribute("target", "_blank");
-          link.setAttribute("rel", "noopener noreferrer");
-      }
+    const exceptionPrefix = "https://liqrase.github.io/";
+    if (
+      link.hostname !== window.location.hostname &&
+      !link.href.startsWith(exceptionPrefix)
+    ) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
   });
 });
