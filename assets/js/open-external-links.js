6fa0b24a8f6b5d63122c9a241bb22e9bdb5bb111
Liqrase <qarnqitana9w4@gmail.com>
2025-04-06 01:36:17 +0900

    Update open-external-links.js

--- assets/js/open-external-links.js
+++ assets/js/open-external-links.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("a").forEach((link) => {
     const exceptionPrefix = "https://liqrase.github.io/";
+    const exceptionPrefix2 = "https://www.liqrase.net/";
     if (
       link.hostname !== window.location.hostname &&
-      !link.href.startsWith(exceptionPrefix)
+      (!link.href.startsWith(exceptionPrefix) && !link.href.startsWith(exceptionPrefix2))
     ) {
       link.setAttribute("target", "_blank");
       link.setAttribute("rel", "noopener noreferrer");
